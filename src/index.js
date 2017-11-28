5d12bece2b1647d65f51aea7c4f2e9a38ee73c5c
sunsu2015 <2435008@163.com>
2017-11-28 23:07:58 +0800

    传统的依赖方式不够明确

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by sunsu on 2017/11/28.
+ */
+function component() {
+    var element = document.createElement('div');
+
+    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    return element;
+}
+
+document.body.appendChild(component());
